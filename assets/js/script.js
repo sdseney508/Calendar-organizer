2f81b8862658e4ecfa61c68bb9068f1175f33511
Steven Seney <sdseney508@gmail.com>
2022-01-22 15:12:38 -0500

    updated the javascript fixing clock time.  Still need to complete the today button and autorow ID's

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,20 @@
-//these are all of the global variables used to set and get the local storage
+//these are all of the global variables used to set and get the local storage.
 
-//use this to make sure we only display the correct number of days in the month and flag if someone tries to select like
-//30 Feb and tell them it is an invalid day.
-var month_day_object = {Jan: 31,
-                    Feb: 28,
-                    Mar: 31,
-                    Apr: 30,
-                    May: 31,
-                    Jun: 30,
-                    Jul: 31,
-                    Aug: 31,
-                    Sep: 30,
-                    Oct: 31,
-                    Nov: 30,
-                    Dec: 31};
-var dayselected; //from  date select form in the navbar of the website
-var monthselected;  //from  date select form in the navbar of the website
-var yearselected;  //from  date select form in the navbar of the website
+var row_time = 600; //initializing my time variable to build the time-rows off of
+var date_sel = $('#date_picker');
+var curr_d = new Date();
+var c_day = curr_d.getDate();
+var c_month = curr_d.getMonth();
+var c_year = curr_d.getFullYear();
+var selected_date;
+var time_now;
+var meeting_cat; //past, present, future are the three options.  set via clock and datepicker
+var curr_time = $('#curr_time');
+var timeblocks = document.querySelector('.time-rows');
+var hourblock; //from the hover over event listener
+var dayselected; //built from datepicker
+var monthselected;  //built from date picker
+var yearselected;  //built from date picker
 var plannedetails = ''; //from the individual time container blocks.
 var meetingtype = ''; //from the pop up form when you select on a time block.
 
@@ -26,44 +24,109 @@ var meetingtype = ''; //from the pop up form when you select on a time block.
 //run the getLocalStorage function.  The same info plus the timeblock, meeting details (plannedetails variable), 
 //and meetingtype is also used to set localstorage for future retrievals.
 
-
-//first i need to populate the dropdown selectors.
-// function makeDateSelector(day_fld, month_fld, year_fld){
-
-function makeDateSelector(){
-    //set up the va
-    var curr_day = new Date();
-    var selected_day = document.getElementById(day_selector)
-    var selected_month = document.getElementById(month_selector)
-    var selected_year = document.getElementById(year_selector)
-    for (var i=0; i<32; i++)
-        dayfield.options[i]=new Option(i, i+1)
-        dayfield.options[today.getDate()]=new Option(today.getDate(), today.getDate(), true, true) //select today's day
-    
-        for (var m=0; m<12; m++)
-        monthfield.options[m]=new Option(monthtext[m], monthtext[m])
-        monthfield.options[today.getMonth()]=new Option(monthtext[today.getMonth()], monthtext[today.getMonth()], true, true) //select today's month
-        var thisyear=today.getFullYear()
-    
-        for (var y=0; y<20; y++){
-    yearfield.options[y]=new Option(thisyear, thisyear)
-    thisyear+=1
+//first i need to populate the dropdown selectors using Jquery ui date picker.  i also want it to compare 
+//if the date selected is in the future or past
+//Set the clock
+function displayDateTime() {
+    time_now = moment().format('HH:HH');
+    hour_now = moment().format('HHHH');
+    // day_now = moment().format("DD");
+    // month_now = moment().format("MM");
+    // date_now = moment().format('MM DD YYYY');
+    console.log("time_now: " + time_now);
+    // console.log("Month now: "+ month_now);
+    // console.log('Long date is: ' + date_now);
+    curr_time.text(time_now);
+    //rebuilds the calendar to change the status of the meetings as you rpogress through the day.  will only change the calendar
+    //if the current day is selected.  i need to add a button to revert the day selected to today.
+    if (hour_now % 100 == 0){
+        buildCalendar(selected_date);
     }
-    yearfield.options[0]=new Option(today.getFullYear(), today.getFullYear(), true, true) //select today's year
+    return hour_now;
 }
 
+date_sel.datepicker({
+    onSelect: function (selected_date, datepicker) {
+        buildCalendar(selected_date);
+        return selected_date;
+    },
+});
+
+console.log(selected_date);
+
+function buildCalendar(t_date) {
+    //update current hour.
+
+    //build the time blocks
+    selected_date = t_date;
+
+    if (selected_date != "") {
+        if (moment(selected_date) > moment()){
+            alert("Date is in the future");
+            meeting_cat = 'future';
+        }
+        else if (moment(selected_date) < moment()){
+            meeting_cat = 'past';
+        }
+        else {
+            meeting_cat = 'present';
+        }
+    }
+    else {
+        selected_date = moment().format('DD MM YY');
+    }
+
+    console.log("the selected date is on line 60: " + selected_date);
+    for (i = 0; i < 13; i++) {
+        //set row time for first block.  initial row time set up top.
+        row_time = Number(row_time) + 100;
+
+        //make ther id 4 characters
+        if (row_time == 800 || row_time == 900 || row_time == 700){
+            row_id = '0' + row_time;
+        }
+        else {
+            row_id = row_time;
+        }
+        //
+        //determine what if any of the row containers will have a class of past or future
 
 
-//contains the functions needed to run the daily calendar application.
 
-//retrieves anything saved to local storage
-function getLocalStorage(dayselected, monthselected, yearselected, timeblock) {
-    p_details = JSON.parse(localStorage.getItem("p_details"));
+        //append a top level row container with a class of past or future, then two children div's 
+        //one for the hour block so they know the time and one for the meeting itself.  
+        $('#time-rows').append('<div class="row"><div class="hour-block col-1">' + row_id + '</div>' +
+        '<div class="meeting-block col-sm-11 col-md-9 col-lg-8 ' + meeting_cat + '"><textarea name=""' +
+        ' id="' + row_id + '" class="input-area">type here</textarea></div></div>');
+        
+        //fill in the meeting details for the time blocks from local storage.
+        document.getElementById
+    }
+
+    //using a loop to assign an event listener to each time block.
+    document.querySelectorAll('.input-area').forEach(item => {
+        item.addEventListener('click', function (event) {
+            event.preventDefault;
+            if (date_sel != null) {
+                alert("The timeblock ID is: " + event.target.id + ' the day of the month is: ' + date_sel);
+            }
+            else {
+                alert("date_sel is null so we used the YYYY of time_now:" + date_sel);
+            }
+        })
+    });
 }
 
-//sets the local storage for a selected timeblock.  inputs are the date,
-//timeblock, entered details, and meeting color.
-function setLocalStorage(dayselected, monthselected, yearselected, timeblock, plannerdetails, meetingtype) {
-    p_details = {date_sel: dayselected, t_block: timeblock, details: plannerdetails, m_type: meetingtype};
-    localStorage.setItem("planneritem", JSON.stringify(p_details));
-}
+// //retrieves anything saved to local storage
+// function getLocalStorage(dayselected, monthselected, yearselected, timeblock) {
+    //     p_details = JSON.parse(localStorage.getItem("p_details"));
+    // }
+    
+    // //sets the local storage for a selected timeblock.  inputs are the date,
+    // //timeblock, entered details, and meeting color.
+    // function setLocalStorage(dayselected, monthselected, yearselected, timeblock, plannerdetails, meetingtype) {
+        //     p_details = { date_sel: dayselected, t_block: timeblock, details: plannerdetails, m_type: meetingtype };
+        //     localStorage.setItem("planneritem", JSON.stringify(p_details));
+        // }
+setInterval(displayDateTime, 1000);
+buildCalendar(selected_date);
