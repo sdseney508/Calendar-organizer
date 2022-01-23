03d5c2a2a270fd7e9a228b4f3196c5d9162342f8
Steven Seney <sdseney508@gmail.com>
2022-01-23 13:02:20 -0500

    Final commit.  Readme updated, all features working properly.

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
 # Calendar-organizer
+
+## Description
+Displays a work day organizer to track details about upcoming meetings and events.  
+  
+## Table of Contents
+-[Installation](#installation)
+
+-[Usage](#usage)
+
+-[Credits](#credits)
+
+-[License](#license)
+
+-[Badges](#badges)
+
+-[Features](#features)
+
+-[How To Contribute](#how_to_contribute)
+
+-[Tests](#tests)
+
+-[Acknowledgements](#acknowledgements)
+
+
+## Installation
+Run the application from the following URL: https://sdseney508.github.io/Javascript-Quiz/
+
+## Usage
+No restrictions on the usage of this calendar tool.
+
+## Credits
+For a good review of formating and descriptions of semantic elements and html and CSS tutorials:  https://www.w3schools.com/
+
+For a good review of formating and descriptions of javascript and tutorials:  https://www.w3schools.com/
+
+Quiz questions found and sourced at:
+htt
+
+## License
+License file located at: https://github.com/sdseney508/calendar-organizer/blob/main/license.txt
+
+## Badges
+N/A for this project.
+
+## Features
+This project contains several features:
+    -jQuery datepicker:  used to select which meeting day the user is looking at.  
+    -set / get localstorage:  All meeting details are saved into local storage so they can be retrieved in the future.  Each meeting block has a unique_ID comprised of the meeting time and meeting date in YYYYMMDD format.  For example, information on a 0800 meeting on 23 Jan 2021 would have a meeting_id of: 080020210123.
+    -Small clock to remind the user of what current time of day is.  It runs off of their computer clock in the local timezone.  No features were added for looking at clocks in other timezones.
+    -Background shading of each meeting block changes based on whether the meeting is in the future or the past.
+    -Revert to current day button:  Resets the meeting display to the current days meetings.
+
+
+## How_to_contribute
+N/A
+
+## Tests
+N/A
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 //these are all of the global variables used to set and get the local storage.
 
 var row_initial = 600; //initializing my time variable to build the time-rows off of
-var date_sel = $('#date_picker');
+var date_sel = $('#date_picker'); 
 var selected_date = moment().format('MM DD YYYY');  //initialize to current date.
 var time_now; //initialize to null value, will be dest by displayDateTime()
 var meeting_cat; //past, present, future are the three options.  set via clock and datepicker
@@ -10,30 +10,22 @@ var meeting_day = $('#meeting-day');
 var timeblocks = document.querySelector('.time-rows');  //used by buildCalendar to set the timeblocks
 var p_details; // name for variable to store info about individual time blocks
 
-//Overview:  
-//Create a navbar with drop down type selectors to get the date for the meetings and organization info.  This opens a small drop down 
-//selector form in the navbar at the top of the page.  It then uses the date info (day, month, year) selected to
-//run the getLocalStorage function.  The same info plus the timeblock, meeting details (plannedetails variable), 
-//and meetingtype is also used to set localstorage for future retrievals.
-
-
-//resets the datepicker to current date and rebuilds the calendar.
 document.getElementById('show-today-btn').addEventListener('click', function(){
+    //resets the datepicker to current date and rebuilds the calendar.
     console.log("i clicked on show today button");
     selected_date = moment().format('MM DD YYYY');
     buildCalendar(selected_date);
     date_sel.datepicker({ dateFormat: 'dd-mm-yy'}).datepicker("setDate", new Date());
 });
 
-//Set the clock
 function displayDateTime() {
+    //Set the clock and show the clock
     time_now = moment().format('HH:mm');
     curr_time.text(time_now);
 }
 
-//first i need to populate the dropdown selectors using Jquery ui date picker.  i also want it to compare 
-//if the date selected is in the future or past
 function initializePage(){
+    //intializes the date picker to current date.
     date_sel.datepicker({ dateFormat: 'mm-dd-yy'}).datepicker("setDate", new Date());
 }
 
@@ -49,27 +41,23 @@ date_sel.datepicker({
 function buildCalendar(t_date) {
     //reset the calendar by setting innerHTML for time-rows = ""
     document.getElementById('time-rows').innerHTML = '';
+
     //build the time blocks
     selected_date = t_date;
-    // console.log('the selected date in calendar builder is: ' + selected_date);
-
-    //set and show the date above the Meeting Blocks
-    // meeting_day.text= selected_date; 
 
     //set row time for the block.  initial row time set up top.
     var row_time = Number(row_initial);
     
     for (i = 0; i < 13; i++) {
         row_time = row_time + 100;
-        //make ther id 4 characters
+        //make the row_id 4 characters
         if (row_time == 800 || row_time == 900 || row_time == 700){
             row_id = '0' + row_time;
         }
         else {
             row_id = row_time;
         }
-    //set the initial past / present / future meeting_cat's for the row colors.  we'll be 
-    //double checking the present day cat's in the next for loop.
+    //set the initial past / present / future meeting_cat's for the row colors. 
     if (moment().format('MM DD YYYY') > selected_date) {
         meeting_cat = 'past';
     }
@@ -77,7 +65,7 @@ function buildCalendar(t_date) {
         meeting_cat = 'future';
     }
     else {
-        //double check meetings for today
+        //set categories for any current day meetings
         var tcheck = moment().format('HHHH');
         if (tcheck - row_id <= 60 && tcheck - row_id >=0){
             meeting_cat = 'present';
@@ -89,11 +77,10 @@ function buildCalendar(t_date) {
             meeting_cat = 'future';
         }
     }   
-        //finish appending the row_ids with the date so we can get / set local storage
+        //set the unique ID's for each row.  the unique IDs get passed to localstorage to track all meetings.
         var year_now = moment(t_date).format("YYYY");
         var month_now = moment(t_date).format("MM");
         var day_now = moment(t_date).format("DD");
-
         meeting_id = row_id + year_now + month_now + day_now;
            
         //append a top level row container with a class of past or future, then two children div's 
@@ -115,9 +102,9 @@ function buildCalendar(t_date) {
     }
 }
     
+function setLocalStorage(meeting_id) {
     //sets the local storage for a selected timeblock.  inputs are the meeting_id as built,
     //in buildCalendar and the meeting details from the textarea.
-function setLocalStorage(meeting_id) {
     meeting_details = document.getElementById(meeting_id).value;
     p_details = [meeting_id, meeting_details];
     localStorage.setItem(meeting_id, JSON.stringify(p_details));
