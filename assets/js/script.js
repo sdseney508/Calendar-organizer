4d1e51a147c96976df8f6231ab5e4e1eacdae98c
Steven Seney <sdseney508@gmail.com>
2022-01-25 21:30:06 -0500

    fixed lack of .past class on days in the past due that was caused by inconsistent cate formats.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 //these are all of the global variables used to set and get the local storage.
-
 var row_initial = 600; //initializing my time variable to build the time-rows off of
-var date_sel = $('#date_picker'); 
+var date_sel = $('#date_picker');
 var selected_date = moment().format('MM DD YYYY');  //initialize to current date.
 var time_now; //initialize to null value, will be dest by displayDateTime()
 var meeting_cat; //past, present, future are the three options.  set via clock and datepicker
@@ -10,12 +9,11 @@ var meeting_day = $('#meeting-day');
 var timeblocks = document.querySelector('.time-rows');  //used by buildCalendar to set the timeblocks
 var p_details; // name for variable to store info about individual time blocks
 
-document.getElementById('show-today-btn').addEventListener('click', function(){
+document.getElementById('show-today-btn').addEventListener('click', function () {
     //resets the datepicker to current date and rebuilds the calendar.
-    console.log("i clicked on show today button");
     selected_date = moment().format('MM DD YYYY');
     buildCalendar(selected_date);
-    date_sel.datepicker({ dateFormat: 'dd-mm-yy'}).datepicker("setDate", new Date());
+    date_sel.datepicker({ dateFormat: 'mm dd yy' }).datepicker("setDate", new Date());
 });
 
 function displayDateTime() {
@@ -24,18 +22,18 @@ function displayDateTime() {
     curr_time.text(time_now);
 }
 
-function initializePage(){
+function initializePage() {
     //intializes the date picker to current date.
-    date_sel.datepicker({ dateFormat: 'mm-dd-yy'}).datepicker("setDate", new Date());
+    date_sel.datepicker({ dateFormat: 'mm dd yy' }).datepicker("setDate", new Date());
 }
 
 date_sel.datepicker({
     onSelect: function (selected_date, datepicker) {
-        console.log('the selected date passed to calendar builder is: ' + selected_date);
-        console.log('the selected date in moment format is: ' + moment(selected_date));
-        buildCalendar(selected_date);
+        //get the date formats consistent across all functions.
+        var sel_date = moment(selected_date).format('MM DD YYYY');
+        buildCalendar(sel_date);
         return selected_date;
-        }
+    }
 });
 
 function buildCalendar(t_date) {
@@ -47,53 +45,55 @@ function buildCalendar(t_date) {
 
     //set row time for the block.  initial row time set up top.
     var row_time = Number(row_initial);
+    var mcheck = moment().format('MM DD YYYY');
     
     for (i = 0; i < 13; i++) {
         row_time = row_time + 100;
         //make the row_id 4 characters
-        if (row_time == 800 || row_time == 900 || row_time == 700){
+        if (row_time == 800 || row_time == 900 || row_time == 700) {
             row_id = '0' + row_time;
         }
         else {
             row_id = row_time;
         }
-    //set the initial past / present / future meeting_cat's for the row colors. 
-    if (moment().format('MM DD YYYY') > selected_date) {
-        meeting_cat = 'past';
-    }
-    else if (moment().format('MM DD YYYY') < selected_date){
-        meeting_cat = 'future';
-    }
-    else {
-        //set categories for any current day meetings
-        var tcheck = moment().format('HHHH');
-        if (tcheck - row_id <= 60 && tcheck - row_id >=0){
-            meeting_cat = 'present';
-        } 
-        else if (tcheck > row_id){
+
+        //set the initial past / present / future meeting_cat's for the row colors. 
+        if (mcheck > selected_date) {
             meeting_cat = 'past';
         }
-        else {
+        else if (mcheck < selected_date) {
             meeting_cat = 'future';
         }
-    }   
+        else {
+            //set categories for any current day meetings
+            var tcheck = moment().format('HHHH');
+            if (tcheck - row_id <= 60 && tcheck - row_id >= 0) {
+                meeting_cat = 'present';
+            }
+            else if (tcheck > row_id) {
+                meeting_cat = 'past';
+            }
+            else {
+                meeting_cat = 'future';
+            }
+        }
         //set the unique ID's for each row.  the unique IDs get passed to localstorage to track all meetings.
         var year_now = moment(t_date).format("YYYY");
         var month_now = moment(t_date).format("MM");
         var day_now = moment(t_date).format("DD");
         meeting_id = row_id + year_now + month_now + day_now;
-           
+
         //append a top level row container with a class of past or future, then two children div's 
         //one for the hour block so they know the time and one for the meeting itself.  
         $('#time-rows').append('<div class="row"><div class="col-1 hour-block">' + row_id + '</div>' +
-        '<div class="meeting-block col-11 ' + meeting_cat + '"><textarea name=""' +
-        ' id="' + meeting_id + '" class="input-area" onfocusout="setLocalStorage(id)"></textarea></div></div>');
-        
+            '<div class="meeting-block col-11 ' + meeting_cat + '"><textarea name=""' +
+            ' id="' + meeting_id + '" class="input-area" onfocusout="setLocalStorage(id)"></textarea></div></div>');
+
         //fill in the meeting details for the time blocks from local storage.
         //here we do the initial pull.  if the pull is null, then we set the meeting block to a null value.
         p_details = JSON.parse(localStorage.getItem(meeting_id));
-        
-        if (p_details != null){
+
+        if (p_details != null) {
             document.getElementById(meeting_id).value = p_details[1];
         }
         else {
@@ -101,7 +101,7 @@ function buildCalendar(t_date) {
         }
     }
 }
-    
+
 function setLocalStorage(meeting_id) {
     //sets the local storage for a selected timeblock.  inputs are the meeting_id as built,
     //in buildCalendar and the meeting details from the textarea.
